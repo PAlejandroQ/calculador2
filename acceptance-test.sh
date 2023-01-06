b641e05232cbbe78e3b0229e55556c0645f8da71
P. Alejandro Quispe <ppaabblloo4283@gmail.com>
2023-01-06 07:38:01 -0500

    Fix calculatod->calculador

--- acceptance-test.sh
+++ acceptance-test.sh
@@ -2,5 +2,5 @@
 set -x
 
 NODE_IP=$(kubectl get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="InternalIP")].address }')
-NODE_PORT=$(kubectl get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
-./gradlew acceptanceTest -Dcalculator.url=http://${NODE_IP}:${NODE_PORT}
+NODE_PORT=$(kubectl get svc calculador-service -o=jsonpath='{.spec.ports[0].nodePort}')
+./gradlew acceptanceTest -D calculator.url=http://${NODE_IP}:${NODE_PORT}
